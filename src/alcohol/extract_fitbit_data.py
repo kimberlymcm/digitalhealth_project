076dcc63055fb975ff69ba839c9992672940656e
Kimberly McManus <kmcmanus@kimberlsmacbook.lan>
2021-07-18 20:29:24 -0700

    Updates to fitbit continuous heart rate grabs

--- src/alcohol/extract_fitbit_data.py
+++ src/alcohol/extract_fitbit_data.py
@@ -1,4 +1,7 @@
 # First run python3 authorize_fitbit.py
+import pandas as pd
+import json
+import time
 
 # Get secrets
 with open("/Users/kmcmanus/Documents/classes/digitalhealth_project/data/keys/fitbit_credentials.txt") as fp:
@@ -18,8 +21,18 @@ okk = Fitbit(client_id=client_id, client_secret=client_secret,
 	refresh_token=refresh_token,
 	expires_at=expires_at)
 
-result = okk.intraday_time_series('activities/heart',
-	base_date='2021-07-16', detail_level='1min',
-	start_time=None, end_time=None)
 
-print(result)
+dates = pd.date_range(start='2021-03-08', end='2021-06-09')
+final_df = pd.DataFrame(columns = ['time', 'value', 'date'])
+for date in dates:
+	result = okk.intraday_time_series('activities/heart',
+		base_date=date, detail_level='1min',
+		start_time=None, end_time=None)
+
+	df_nested_list = pd.json_normalize(result['activities-heart-intraday'], record_path =['dataset'])
+	df_nested_list["date"] = date
+	final_df.append(df_nested_list)
+	time.sleep(3)
+
+
+final_df.to_csv("/Users/kmcmanus/Documents/classes/digitalhealth_project/data/alcohol/hr_data_20210308_20210609_1min.csv")
